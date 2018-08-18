dc88fc1b39d0ee14dcf1e468f27444889cfed4c4
Sahil Nanda <sahilsahilkumar1234@gmail.com>
2018-08-18 21:21:10 +0530

    some changes

---
+++ onboarding/src/pages/odisha/odisha.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { OdishaPage } from './odisha';
+
+@NgModule({
+  declarations: [
+    OdishaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(OdishaPage),
+  ],
+})
+export class OdishaPageModule {}
